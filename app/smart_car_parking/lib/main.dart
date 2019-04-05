10741849cc38f3927d1f2b93c82375b97e01e140
Sahil Patel <goal.sahil6@gmail.com>
2019-04-05 17:12:47 +0530

    Add files via upload

---
+++ app/smart_car_parking/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Car Parking',
+      theme: ThemeData.dark(),
+      home: HomePage(title: 'Smart Car Parking'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final String title;
+  HomePage({this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: Center(child: Text("Hello World")),
+    );
+  }
+}
