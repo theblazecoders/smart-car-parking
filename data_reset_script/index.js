5a2bcf04350110f4d4c3785bb33d73b767bae18d
Sahil Patel <goal.sahil6@gmail.com>
2019-04-06 18:30:58 +0530

    Reset Script Added

---
+++ data_reset_script/index.js
@@ -0,0 +1,55 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./admin.json");
+
+const numberOfTowers = 6;
+
+const numberOfParkingSlots = 6;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://smart-car-parking-5dfda.firebaseio.com"
+});
+
+
+var db = admin.firestore();
+
+var towersInfo = db.collection("towersInfo");
+
+towersInfo.doc("occupancy").set({
+    "occupancy":0
+});
+
+towersInfo.doc("numberOfTowers").set({
+    "numberOfTowers":numberOfTowers
+});
+
+towersInfo.doc("totalSize").set({
+    "occupancy": numberOfTowers * numberOfParkingSlots
+});
+
+var towers = [];
+
+for(var i=0;i<numberOfTowers;i++){
+
+    var parkingSlots = [];
+    for(var j =0 ;j < numberOfParkingSlots;j++){
+        parkingSlots.push({
+            "index": j,
+            "isOccupied":false,
+            "occupiedTimeStamp":null,
+            "ownerName":"",
+            "ownerEmailID": "",
+            "password":"",
+        })
+    }
+    towers.push({
+        "location" : 0,
+        "occupied":0,
+        "size":numberOfParkingSlots,
+        "parkingSlots": parkingSlots,
+    })
+}
+towersInfo.doc("towers").set({"towers":towers});
+
