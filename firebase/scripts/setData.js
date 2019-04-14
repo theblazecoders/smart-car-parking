f2b115af169dba9f01a84d4d02fb599caf2e1309
HarshKhandeparkar <mjkgoa@gmail.com>
2019-04-14 13:31:37 +0530

    setData

---
+++ firebase/scripts/setData.js
@@ -0,0 +1,1 @@
+// code yet to be added
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
   firebase: {
-    resetData: require('./firebase/scripts/resetData')
+    resetData: require('./firebase/scripts/resetData'),
+    getData: require('./firebase/scripts/getData'),
+    setData: require('./firebase/scripts/setData')
   }
 }
