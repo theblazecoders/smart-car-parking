12cd92bda99872c9b94d4588c41bba5eac775e26
HarshKhandeparkar <mjkgoa@gmail.com>
2019-04-26 15:18:03 +0530

    functions

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,6 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!")
-})
-
 exports.getData = functions.https.onRequest((req, res) => {
   const db = admin.firestore()
   return db.collection('towersInfo').doc('mainInfo').get()
